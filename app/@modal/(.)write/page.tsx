718b4de77203abe248a464c966947d1161f16d4c
Sarino MALALANIRINA <sarino.malalanirina@esti.mg>
2024-05-17 16:16:27 +0300

    Feat : permettre d'afficher un modal pour répondre un post.

--- app/@modal/(.)write/page.tsx
+++ app/@modal/(.)write/page.tsx
@@ -1,7 +1,7 @@
 import { getUser } from '@/src/query/user.query'
 import React from 'react'
-import WriteModal from './WriteModal';
 import { createPost } from '@/app/write/write-post.action';
+import WriteModal from './WriteModal';
 
 export default async function Page() {
     const user = await getUser();
